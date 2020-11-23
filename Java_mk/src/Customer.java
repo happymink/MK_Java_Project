a7828adbe8bd186bec468cfc2a92d0fa49d1988b
happymink <71982555+happymink@users.noreply.github.com>
2020-11-23 22:45:50 +0900

    1

---
+++ Java_mk/src/Customer.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+
+public class Customer {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static Connection getConnection() {
+		try {
+		
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+}
